1f67dc1eea04be5c09ab6587134a5a434b9868de
ricassiocosta <ricassiocosta@tuta.io>
2021-02-14 11:57:18 -0300

    game logic started

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 new Vue({
   el: '#app',
   data: {
-    
+    playerDot: "",
+    iaDot: "",
+    playTime: "",
+    boardVisibility: "hidden",
+    menuVisibility: "",
+    cell_1a: "",
+    cell_2a: "",
+    cell_3a: "",
+    cell_1b: "",
+    cell_2b: "",
+    cell_3b: "",
+    cell_1c: "",
+    cell_2c: "",
+    cell_3c: "",
   },
 	methods: {
+    setPlayerDot: function(dot) {
+      this.playerDot = dot
+      if (this.playerDot == 'x') {
+        this.iaDot = 'circle'
+      } else {
+        this.iaDot = 'x'
+      }
 
+      this.startGame()
+    },
+    startGame: function() {
+      this.boardVisibility = "visible"
+      this.menuVisibility = "hidden"
+    },
+
+    play: function(cell) {
+      this[cell] = `/assets/${this.playerDot}.svg`
+    }
 	}
 })
